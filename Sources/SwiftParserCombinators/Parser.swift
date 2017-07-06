18c3e14c0374734d2056b19f3446e57240b76781
Bastian Müller <bastian@turbolent.com>
2017-07-06 07:31:46 -0700

    add error parse result

--- Sources/SwiftParserCombinators/Parser.swift
+++ Sources/SwiftParserCombinators/Parser.swift
@@ -243,14 +243,20 @@ func rep<T, Input>(_ parser: @autoclosure @escaping () -> Parser<T, Input>, min:
 
             let result = lazyParser.value.parse(remaining)
             switch result {
-            case .success(let value, let rest):
+            case let .success(value, rest):
                 elements.append(value)
                 n += 1
                 remaining = rest
-            case .failure(let message, let remaining2):
+            case let .failure(message2, remaining2):
                 guard n >= min else {
                     // NOTE: unfortunately Swift doesn't have a bottom type, so can't use `result` here.
-                    return .failure(message: message, remaining: remaining2)
+                    return .failure(message: message2, remaining: remaining2)
+                }
+                return .success(value: elements, remaining: remaining)
+            case let .error(message2, remaining2):
+                guard n >= min else {
+                    // NOTE: unfortunately Swift doesn't have a bottom type, so can't use `result` here.
+                    return .error(message: message2, remaining: remaining2)
                 }
                 return .success(value: elements, remaining: remaining)
             }
