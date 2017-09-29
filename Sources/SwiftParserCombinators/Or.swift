af4aa9e150e8663171330ce1f1ee708494ece712
Bastian Müller <bastian@turbolent.com>
2017-09-28 21:38:43 -0700

    remove 'append'

--- Sources/SwiftParserCombinators/Or.swift
+++ Sources/SwiftParserCombinators/Or.swift
@@ -2,16 +2,11 @@
 extension Parser {
 
     // NOTE: unfortunately it is not possible in Swift to constrain U to be a supertype of T
-    public func append<U>(_ next: @autoclosure @escaping () -> Parser<U, Input>) -> Parser<U, Input> {
+    public func or<U>(_ next: @autoclosure @escaping () -> Parser<U, Input>) -> Parser<U, Input> {
         let lazyNext = Lazy(next)
         return Parser<U, Input> { input in
             self.step(input).flatMap { $0.append(lazyNext.value.step(input)) }
         }
-    }
-
-    // NOTE: unfortunately it is not possible in Swift to constrain U to be a supertype of T
-    public func or<U>(_ next: @autoclosure @escaping () -> Parser<U, Input>) -> Parser<U, Input> {
-        return append(next())
     }
 }
 
@@ -23,3 +18,4 @@ public func || <T, U, Input>(lhs: @autoclosure () -> Parser<T, Input>,
 {
     return lhs().or(rhs())
 }
+
