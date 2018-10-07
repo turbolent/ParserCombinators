e842c5bdddbf0ec10a8debfac5ecda0e08d1777c
Bastian Müller <bastian@turbolent.com>
2018-10-06 23:23:31 -0700

    document skipUntil

--- Sources/ParserCombinators/SkipUntil.swift
+++ Sources/ParserCombinators/SkipUntil.swift
@@ -6,22 +6,29 @@ private func skipUntilStep<T, Element>(parser: Parser<T, Element>,
     -> Trampoline<ParseResult<T, Element>>
 {
     return More {
-        return parser.step(remaining).flatMap { result in
-            switch result {
-            case .success, .error:
-                return Done(result)
-            case .failure(_, let remaining):
-                guard !remaining.atEnd else {
-                    return Done(.failure(message: "end of input",
-                                         remaining: remaining))
+        return parser.step(remaining)
+            .flatMap { result in
+                switch result {
+                case .success, .error:
+                    return Done(result)
+                case .failure(_, let remaining):
+                    guard !remaining.atEnd else {
+                        return Done(.failure(message: "end of input",
+                                             remaining: remaining))
+                    }
+                    return skipUntilStep(parser: parser,
+                                         remaining: remaining.rest())
                 }
-                return skipUntilStep(parser: parser,
-                                     remaining: remaining.rest())
             }
-        }
     }
 }
 
+/// Creates a new parser from the given parser that succeeds if the given parser succeeds.
+/// Any any input preceeding the matching input is ignored.
+///
+/// - Parameters:
+///   - parser: The parser to be applied.
+///
 public func skipUntil<T, Element>(_ parser: @autoclosure @escaping () -> Parser<T, Element>)
     -> Parser<T, Element>
 {
